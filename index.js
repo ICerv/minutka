09401d5243b99474172bdbf505f85f31d10791ae
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-12 02:27:16 +0200

    setTimeout/clearTimeout

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('funguju!');
+
+const userSetTime = Number(prompt('Zadej pocet sekund:') * 1000);
+const audio = document.querySelector('audio');
+const btnElm = document.querySelector('button');
+
+const ring = () => {
+  const alarmElm = document.querySelector('.alarm');
+  alarmElm.classList.add('alarm--ring')
+  audio.play();
+}
+
+const deduction = setTimeout(ring, userSetTime);
+
+
+btnElm.addEventListener('click', () => {
+  clearTimeout(deduction);
+});
+
